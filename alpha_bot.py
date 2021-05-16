096a4643a571e4f2fa3629924d852f99f1bfcdb2
Ben Morrall <bemo56@hotmail.com>
2021-05-16 18:07:18 +1000

    Move worker all-in into method

--- alpha_bot.py
+++ alpha_bot.py
@@ -5,8 +5,12 @@ from sc2.player import Bot, Computer
 class AlphaBot(sc2.BotAI):
     async def on_step(self, iteration):
         if iteration == 0:
-            for worker in self.workers:
-                await self.do(worker.attack(self.enemy_start_locations[0]))
+            await self.worker_attack();
+
+    # Attack with all Workers
+    async def worker_attack(self):
+        for worker in self.workers:
+            await self.do(worker.attack(self.enemy_start_locations[0]))
 
 run_game(maps.get("Simple64"), [
     Bot(Race.Terran, AlphaBot()),
