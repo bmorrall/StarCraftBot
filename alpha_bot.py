c42daa0cfd146caee53f7010bad9beceb1a86656
Ben Morrall <bemo56@hotmail.com>
2021-05-16 21:26:44 +1000

    Build barracks at choke

--- alpha_bot.py
+++ alpha_bot.py
@@ -1,7 +1,8 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from sc2.constants import COMMANDCENTER, SCV, SUPPLYDEPOT, SUPPLYDEPOTLOWERED, MORPH_SUPPLYDEPOT_LOWER, MORPH_SUPPLYDEPOT_RAISE
+from sc2.constants import COMMANDCENTER, SCV, SUPPLYDEPOT, BARRACKS, \
+    SUPPLYDEPOTLOWERED, MORPH_SUPPLYDEPOT_LOWER, MORPH_SUPPLYDEPOT_RAISE
 
 
 class AlphaBot(sc2.BotAI):
@@ -9,11 +10,12 @@ class AlphaBot(sc2.BotAI):
         await self.distribute_workers()  # in sc2/bot_ai.py
         await self.build_workers()
         await self.build_supply_depot()
+        await self.build_barracks()
         await self.raise_lower_depots()
         await self.expand()
 
     async def build_workers(self):
-        ideal = 1 # one for construction
+        ideal = 1  # one for construction
         for cc in self.units(COMMANDCENTER):
             ideal += cc.ideal_harvesters
         # Add pending command center units count
@@ -25,8 +27,7 @@ class AlphaBot(sc2.BotAI):
 
     async def build_supply_depot(self):
         # Find all urgently required depot locations
-        depot_placement_positions = self.main_base_ramp.corner_depots | {
-            self.main_base_ramp.depot_in_middle}
+        depot_placement_positions = self.main_base_ramp.corner_depots
         depots = self.units(SUPPLYDEPOT) | self.units(SUPPLYDEPOTLOWERED)
         if depots:
             depot_placement_positions = {
@@ -43,9 +44,17 @@ class AlphaBot(sc2.BotAI):
             if self.can_afford(SUPPLYDEPOT) and self.units(COMMANDCENTER).ready:
                 await self.build(SUPPLYDEPOT, near=depots.last)
 
+    async def build_barracks(self):
+        # barracks_placement_position = self.main_base_ramp.barracks_correct_placement
+        barracks_placement_position = self.main_base_ramp.barracks_in_middle
+        if self.can_afford(BARRACKS) and not self.already_pending(BARRACKS):
+            if self.units(BARRACKS).amount + self.already_pending(BARRACKS) > 0:
+                return
+            await self.build(BARRACKS, barracks_placement_position)
 
     async def expand(self):
-        cc_count = self.units(COMMANDCENTER).amount + self.already_pending(COMMANDCENTER)
+        cc_count = self.units(COMMANDCENTER).amount + \
+            self.already_pending(COMMANDCENTER)
         if cc_count < 2 and self.can_afford(COMMANDCENTER):
             await self.expand_now()
 
