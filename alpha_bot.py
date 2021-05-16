531114f6341ee430280797df6f537f3f6ea0537c
Ben Morrall <bemo56@hotmail.com>
2021-05-16 18:10:29 +1000

    Add worker and supply depot construction

--- alpha_bot.py
+++ alpha_bot.py
@@ -1,16 +1,32 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
+from sc2.constants import COMMANDCENTER, SCV, SUPPLYDEPOT
+
 
 class AlphaBot(sc2.BotAI):
     async def on_step(self, iteration):
-        if iteration == 0:
-            await self.worker_attack();
+        await self.distribute_workers()  # in sc2/bot_ai.py
+        await self.build_workers()
+        await self.build_supply_depot()
+
+    async def build_workers(self):
+        for command_center in self.units(COMMANDCENTER).idle:
+            if self.can_afford(SCV):
+                await self.do(command_center.train(SCV))
+
+    async def build_supply_depot(self):
+        if self.supply_left < 5 and not self.already_pending(SUPPLYDEPOT):
+            command_centers = self.units(COMMANDCENTER).ready
+            if command_centers.exists:
+                if self.can_afford(SUPPLYDEPOT):
+                    await self.build(SUPPLYDEPOT, near=command_centers.first)
 
     # Attack with all Workers
     async def worker_attack(self):
         for worker in self.workers:
             await self.do(worker.attack(self.enemy_start_locations[0]))
+
 
 run_game(maps.get("Simple64"), [
     Bot(Race.Terran, AlphaBot()),
